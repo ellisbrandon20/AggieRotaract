08d3643f14ddaa87918d955c9a39d7883d7ba1d2
Matthew Ball <matthew_ball@tamu.edu>
2017-02-27 17:42:08 -0600

    got rid of modules in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,4 @@
 class CreateUsers < ActiveRecord::Migration
-	module Classification
-		1 = "Freshman"
-		2 = "Sophomore"
-		3 = "Junior"
-		4 = "Senior"
-		5 = "GradStudent"
-	end
-
-	module Shirt
-		1 = "XS"
-		2 = "S"
-		3 = "M"
-		4 = "L"
-		5 = "XL"
-		6 = "XXL"
-		7 = "XXXL"
-	end
-
 	def change
     create_table :users do |t|
     	t.integer :UIN
@@ -24,9 +6,9 @@ class CreateUsers < ActiveRecord::Migration
     	t.integer :phone
     	t.string :email
     	t.text :address
-    	t.Classification :classification
+    	t.string :classification
     	t.string :major
-    	t.Shirt :shirt
+    	t.string :shirt
     	t.boolean :gender
     	t.boolean :officer
     	t.boolean :active
