4abed2f9396681587650e81efe9af6c37c8ea8bd
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-02-27 20:32:55 +0000

    added dashboard controller to make a 'dashboard' page for both admin and member

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'events/index'
+    get 'dashboard/index'
     
-    root 'events#show'
+    root 'dashboard#index'
     
     resources :events
 end
