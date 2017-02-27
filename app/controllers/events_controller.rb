db515a80b9e78d20735ad371c102e24b23194471
Nathan Barker <nbarker@outlook.com>
2017-02-27 23:07:35 +0000

    Added event params and show html page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,9 +3,18 @@ class EventsController < ApplicationController
     end
     
     def create
-        @event = Event.new(params[:event])
+        @event = Event.new(event_params)
         
         @event.save
         redirect_to @event
     end
+    
+    def show
+        @event = Event.find(params[:id])
+    end
+    
+    private
+        def event_params
+            params.require(:event).permit(:name, :descr)
+        end
 end
