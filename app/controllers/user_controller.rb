da5f5637cf8aeccc0e89f544df76055763bf336c
Matthew Ball <matthew_ball@tamu.edu>
2017-03-01 22:14:16 -0600

    rename CreateUsers to User

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,6 @@
 class UserController < ApplicationController
 	def login
-		puts("---------*********")
-		puts(params[:UIN])
+		
 	end
 
 	def add_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class CreateUsers < ActiveRecord::Migration
+class User < ActiveRecord::Migration
 	def change
     create_table :users do |t|
     	t.integer :UIN
