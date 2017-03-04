383d74777f2b5deeb1a4e2262ea2d4049b10bd42
Jared Russell <jarheadr176@tamu.edu>
2017-03-04 22:19:31 +0000

    Adding datepicker and updating event saving

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,21 @@ class EventsController < ApplicationController
     def new
     end
     
+    
+    
     def create
-        @event = Event.new(event_params)
+        #@event = Event.new(event_params)
+        
+        @event = Event.create!(:name => event_params[:name],
+                 :description => event_params[:description],
+                 :address => event_params[:address],
+                 :meeting => event_params[:meeting],
+                 :date => event_params[:date],
+                 :start_time => event_params[:start_time],
+                 :end_time => event_params[:end_time],
+                 :max_points => event_params[:max_points],
+                 :contact => event_params[:contact],
+                 :image => event_params[:image])
         
         @event.save
         redirect_to @event
@@ -12,6 +25,7 @@ class EventsController < ApplicationController
     def show
         @event = Event.find(params[:id])
     end
+    
     
     private
         def event_params
