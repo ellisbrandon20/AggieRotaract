d4135412ea2567353059a305927cc571fc090381
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-03-04 23:01:09 +0000

    fixed bugs with meeting and date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,18 @@
 class EventsController < ApplicationController
+    require 'date'
+    
     def new
     end
     
-    
-    
     def create
+        date_arr = event_params[:date].split('/')
+        date = date_arr[2] + '-' + date_arr[0] + '-' + date_arr[1]
+        
         @event = Event.create!(:name => event_params[:name],
                  :description => event_params[:description],
                  :address => event_params[:address],
-                 :meeting => event_params[:meeting],
-                 :date => event_params[:date],
+                 :meeting => event_params[:meeting] == '1' ? true : false,
+                 :date => date,
                  :start_time => event_params[:start_time],
                  :end_time => event_params[:end_time],
                  :max_points => event_params[:max_points],
