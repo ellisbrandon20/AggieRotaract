ecaa46f10fe9433195cb0271fe989f69cee0e50c
Jared Russell <jarheadr176@tamu.edu>
2017-03-08 22:44:15 +0000

    Adding display from events controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,9 @@
 class AttendancesController < ApplicationController
+    def index
+        curr_time = DateTime.now.to_date
+        @all_events = Event.all
+        @upcoming_events = @all_events.where("date >= :date and meeting = :meeting", {date: curr_time, meeting: [false]})
+        @upcoming_events_email = @upcoming_events
+        contact_uin_to_email
+    end
 end
