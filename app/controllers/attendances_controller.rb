3fc472b255c6a3a689f4f1922a10b08f2425b02d
Jared Russell <jarheadr176@tamu.edu>
2017-03-08 22:46:44 +0000

    Adding fix to attendances controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,6 +4,6 @@ class AttendancesController < ApplicationController
         @all_events = Event.all
         @upcoming_events = @all_events.where("date >= :date and meeting = :meeting", {date: curr_time, meeting: [false]})
         @upcoming_events_email = @upcoming_events
-        contact_uin_to_email
+        #contact_uin_to_email
     end
 end
