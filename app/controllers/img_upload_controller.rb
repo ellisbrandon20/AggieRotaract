a54a872f84892652235a79f3c35a036ad9b3aa36
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-03-08 02:13:47 +0000

    img_uploader controller spec

--- app/controllers/img_upload_controller.rb
+++ app/controllers/img_upload_controller.rb
@@ -3,13 +3,19 @@ class ImgUploadController < ApplicationController
   def new
 
   end
-
+  
   def create
-    name = params[:upload][:file].original_filename
+    
+    upload=params[:upload]
+    name = upload[:file].original_filename
+    
     directory = "app/assets/images"
     path = File.join(directory, name)
-    File.open(path, "wb") { |f| f.write(params[:upload][:file].read) }
+    
+    File.open(path, "wb") { |f| f.write(upload[:file].read) }
+    
     flash[:notice] = "File uploaded"
     session[:image_upload] = name
+    
   end
 end
--- spec/controllers/img_upload_controller_spec.rb
+++ spec/controllers/img_upload_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ImgUploadController, type: :controller do
 
+    before :each do
+        @file = fixture_file_upload('files/test.jpg', 'images/jpg')
+    end
+    
+    it "can upload a image" do
+        file = Hash.new
+        file[:file] = @file
+        post :create, :upload => file
+        response.should be_success
+    end
+    
 end
