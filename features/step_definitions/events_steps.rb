75079a80616b4bb8fffadd5b2427d0a97b726b52
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-03-08 03:07:36 +0000

    finished view_events.feature

--- features/step_definitions/events_steps.rb
+++ features/step_definitions/events_steps.rb
@@ -8,24 +8,14 @@ Then(/^I should see events$/) do
   page.has_xpath?("//div[@id='event-card']")
 end
 
-Then(/^I should see "([^"]*)"$/) do |arg1|
-    # I cant figure out how to grab the event_title tag and the value of it to check this
-    # puts("--------------")
-    # page.find(:xpath, "//div[@id='event_title']")
-    # puts("--------------")
-    # find_by_id("event_title").native.text == arg1
+# Then(/^I should see "([^"]*)"$/) do |arg1|
+#     # I cant figure out how to grab the event_title tag and the value of it to check this
+#     # puts("--------------")
+#     # page.find(:xpath, "//div[@id='event_title']")
+#     # puts("--------------")
+#     # find_by_id("event_title").native.text == arg1
     
-end
-
-Given(/^I have events titled (.+)$/) do |events|
-    events.split(", ").each do |event|
-        Event.create!(:name => event)
-    end
-end
-
-Then(/^I should see events$/) do
-  page.has_xpath?("//div[@id='event-card']")
-end
+# end
 
 When(/^event information is inputted$/) do
   fill_in 'name_id', :with => 'Event'  #event name
