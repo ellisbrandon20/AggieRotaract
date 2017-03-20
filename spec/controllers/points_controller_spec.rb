2197ca8ee1d26932813940ad39213c2123b858e1
Carlos Cardin <ccardin9@tamu.edu>
2017-03-20 22:16:22 +0000

    added rspec test for meeting_signin

--- spec/controllers/points_controller_spec.rb
+++ spec/controllers/points_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+describe PointsController do
+    describe 'signing in to meeting' do
+        it 'should add user to roster and give them points' do
+            expect {
+                post :meeting_signin
+            }.not_to raise_error
+        end
+    end
+end
+
 RSpec.describe PointsController, type: :controller do
 
 end
