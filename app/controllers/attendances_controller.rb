7c766ce0730272b44e43ef2f4f8b917f122fa86d
Matthew Ball <matthew_ball@tamu.edu>
2017-03-24 15:27:34 -0500

    add new and create actions for attendance_controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,21 @@
 class AttendancesController < ApplicationController
+	def index
+	end
+
+	def new
+		@attendance = Attendance.new
+		@attendance.user = current_user
+		@attendance.event = Attendance.find(params[:event_id])
+	end
+
+	def create
+		@attendance = Attendance.new
+    	@attendance.user = current_user
+    	@attendance.event = Attendance.find(params[:event_id])
+    	if @attendance.save
+      		redirect_to events_path, notice: "You are registered!"
+    	else
+      		render action: 'new'
+    	end
+	end
 end
