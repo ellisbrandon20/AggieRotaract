d8fe8bd7ef29592aa5b53d57196386ae0eb8b4e2
Matthew Ball <matthew_ball@tamu.edu>
2017-03-24 13:53:39 -0500

    add participation schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308213057) do
+ActiveRecord::Schema.define(version: 20170324183254) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "UIN"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20170308213057) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "participations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "points", force: :cascade do |t|
