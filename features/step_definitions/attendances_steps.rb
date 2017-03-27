5d6682ab7acd246246ddfb646cca4a629e6d912b
Jared Russell <jarheadr176@tamu.edu>
2017-03-27 21:34:29 +0000

    Fixing the cucumber test for the dashboard

--- features/step_definitions/attendances_steps.rb
+++ features/step_definitions/attendances_steps.rb
@@ -2,11 +2,18 @@ Given(/^I have events titled (.+)$/) do |events|
     
 end
 
-Then(/^I should see "([^\"]*)"$/) do |text|
+Given(/^I have events titled (.+)$/) do |events|
+    
+end
+
+Given(/^I log in/) do
+     visit("/")
+     fill_in 'login_UIN_id', :with => "123456789"
+     click_button("login_button_id")
+end
+
+Then(/^I am going to events/) do
      #page.should have_content(text)
      #page.find_by_id("event_title").text(text)
  end
  
- When(/^I visit "([^"]*)"$/) do |arg1|
-  visit(arg1)
-end
