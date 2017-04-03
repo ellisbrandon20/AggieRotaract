70d4c777072e4deff51b63e16cdff5fb56252321
Matthew Ball <matthew_ball@tamu.edu>
2017-04-03 17:28:54 -0500

    Add seed for Points table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,22 @@
 
 # Iteration 1
             
-            # events = [
-            #           {:name => 'Big Event', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
-            #           :description => 'Since its introduction in 1982, The Big Event has become the largest, one-day, student-run service project in the nation. Each spring, tens of thousands of Texas A&M students come together to say "Thank You" to the residents of Bryan and College Station. For the past 33 years, Aggie students have participated in this annual event to show their appreciation to the surrounding community, completing service projects such as yard work, window washing, and painting for residents of the community. Although The Big Event has become the largest one-day, student-run service project in the nation, our message and our mission remains the same – to simply say “Thank You.”',
-            #           :date => '2017-07-15',:start_time => '10:00:00', :end_time => '15:00:00', :capacity => 30, :max_points => 3.0, 
-            #           :meeting => false, :contact => 123456789, :image => 'big_event.jpeg'},
+            events = [
+                      {:name => 'Big Event', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
+                      :description => 'Since its introduction in 1982, The Big Event has become the largest, one-day, student-run service project in the nation. Each spring, tens of thousands of Texas A&M students come together to say "Thank You" to the residents of Bryan and College Station. For the past 33 years, Aggie students have participated in this annual event to show their appreciation to the surrounding community, completing service projects such as yard work, window washing, and painting for residents of the community. Although The Big Event has become the largest one-day, student-run service project in the nation, our message and our mission remains the same – to simply say “Thank You.”',
+                      :date => '2017-07-15',:start_time => '10:00:00', :end_time => '15:00:00', :capacity => 30, :max_points => 3.0, 
+                      :meeting => false, :contact => 123456789, :image => 'big_event.jpeg'},
                        
-            #           {:name => 'Habitat for Humanity', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
-            #           :description => 'Aggie Habitat for Humanity is the Texas A&M Campus Chapter of Habitat for Humanity International. We work closely with Bryan/College Station Habitat for Humanity.',
-            #           :date => '2017-07-16', :start_time => '09:00:00', :end_time => '13:00:00', :capacity => 25, :max_points => 2.0, 
-            #           :meeting => false, :contact => 987654321, :image => 'habitat_for_humanity.jpeg'},
-            #         ]
+                      {:name => 'Habitat for Humanity', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
+                      :description => 'Aggie Habitat for Humanity is the Texas A&M Campus Chapter of Habitat for Humanity International. We work closely with Bryan/College Station Habitat for Humanity.',
+                      :date => '2017-07-16', :start_time => '09:00:00', :end_time => '13:00:00', :capacity => 25, :max_points => 2.0, 
+                      :meeting => false, :contact => 987654321, :image => 'habitat_for_humanity.jpeg'},
+                    ]
                      
             
-            # events.each do |event|
-            #     Event.create!(event)
-            # end
+            events.each do |event|
+                Event.create!(event)
+            end
             
             # users = [
             #   {:UIN => 123456789, :name => "Carlos Cardin", :phone => "1234567890", :email => "me@tamu.edu", 
@@ -77,6 +77,17 @@ users = [
 
 users.each do |user|
     User.create!(user)
+end
+
+
+points = [
+  {:event_id => 1, :points => 37, :UIN => 123456789, :issue_date => '2017-01-15'},
+  {:event_id => 2, :points => 56, :UIN => 123456789, :issue_date => '2017-01-18'},
+  {:event_id => 3, :points => 11, :UIN => 123456789, :issue_date => '2017-01-20'},
+]
+
+points.each do |point|
+  Point.create!(point)
 end
 
 # attendances = [
