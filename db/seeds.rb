127d215a03ec8d32dafdfaf83ed8c6c1d25ce2d8
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-04-09 18:39:15 +0000

    pulling from master

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,32 @@
 
 # Iteration 1
             
-            # events = [
-            #           {:name => 'Big Event', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
-            #           :description => 'Since its introduction in 1982, The Big Event has become the largest, one-day, student-run service project in the nation. Each spring, tens of thousands of Texas A&M students come together to say "Thank You" to the residents of Bryan and College Station. For the past 33 years, Aggie students have participated in this annual event to show their appreciation to the surrounding community, completing service projects such as yard work, window washing, and painting for residents of the community. Although The Big Event has become the largest one-day, student-run service project in the nation, our message and our mission remains the same – to simply say “Thank You.”',
-            #           :date => '2017-07-15',:start_time => '10:00:00', :end_time => '15:00:00', :capacity => 30, :max_points => 3.0, 
-            #           :meeting => false, :contact => 123456789, :image => 'big_event.jpeg'},
+            events = [
+                      {:name => 'Big Event', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
+                      :description => 'Since its introduction in 1982, The Big Event has become the largest, one-day, student-run service project in the nation. Each spring, tens of thousands of Texas A&M students come together to say "Thank You" to the residents of Bryan and College Station. For the past 33 years, Aggie students have participated in this annual event to show their appreciation to the surrounding community, completing service projects such as yard work, window washing, and painting for residents of the community. Although The Big Event has become the largest one-day, student-run service project in the nation, our message and our mission remains the same – to simply say “Thank You.”',
+                      :date => '2017-07-15',:start_time => '10:00:00', :end_time => '15:00:00', :capacity => 30, :max_points => 3.0, 
+                      :meeting => false, :contact => 123456789, :image => 'big_event.jpeg'},
                        
-            #           {:name => 'Habitat for Humanity', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
-            #           :description => 'Aggie Habitat for Humanity is the Texas A&M Campus Chapter of Habitat for Humanity International. We work closely with Bryan/College Station Habitat for Humanity.',
-            #           :date => '2017-07-16', :start_time => '09:00:00', :end_time => '13:00:00', :capacity => 25, :max_points => 2.0, 
-            #           :meeting => false, :contact => 987654321, :image => 'habitat_for_humanity.jpeg'},
-            #         ]
+                      {:name => 'Habitat for Humanity', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
+                      :description => 'Aggie Habitat for Humanity is the Texas A&M Campus Chapter of Habitat for Humanity International. We work closely with Bryan/College Station Habitat for Humanity.',
+                      :date => '2017-07-16', :start_time => '09:00:00', :end_time => '13:00:00', :capacity => 25, :max_points => 2.0, 
+                      :meeting => false, :contact => 987654321, :image => 'habitat_for_humanity.jpeg'},
+                      
+                      {:name => 'past event 1', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
+                      :description => 'Since its introduction in 1982, The Big Event has become the largest, one-day, student-run service project in the nation. Each spring, tens of thousands of Texas A&M students come together to say "Thank You" to the residents of Bryan and College Station. For the past 33 years, Aggie students have participated in this annual event to show their appreciation to the surrounding community, completing service projects such as yard work, window washing, and painting for residents of the community. Although The Big Event has become the largest one-day, student-run service project in the nation, our message and our mission remains the same – to simply say “Thank You.”',
+                      :date => '2017-01-01',:start_time => '10:00:00', :end_time => '15:00:00', :capacity => 30, :max_points => 3.0, 
+                      :meeting => false, :contact => 123456789, :image => 'big_event.jpeg'},
+                       
+                      {:name => 'past event 2', :address => 'Administration Building, 400 Bizzell St, College Station, TX 77843',
+                      :description => 'Aggie Habitat for Humanity is the Texas A&M Campus Chapter of Habitat for Humanity International. We work closely with Bryan/College Station Habitat for Humanity.',
+                      :date => '2017-01-01', :start_time => '09:00:00', :end_time => '13:00:00', :capacity => 25, :max_points => 2.0, 
+                      :meeting => false, :contact => 987654321, :image => 'habitat_for_humanity.jpeg'}
+                    ]
                      
             
-            # events.each do |event|
-            #     Event.create!(event)
-            # end
+            events.each do |event|
+                Event.create!(event)
+            end
             
             # users = [
             #   {:UIN => 123456789, :name => "Carlos Cardin", :phone => "1234567890", :email => "me@tamu.edu", 
@@ -96,3 +106,20 @@ end
 # attendances.each do |attendance|
 #     Attendance.create!(attendance)
 # end
+
+
+attendances = [
+    
+    {:UIN => 987654321, :car_ride => 0, :comments => "more fun fun fun", :wait_listed => 0, :event_id => 3, :time_stamp => '2017-03-26 19:37:13', :approved => 0},
+    {:UIN => 123456789, :car_ride => 0, :comments => "fun fun fun", :wait_listed => 0, :event_id => 3, :time_stamp => '2017-03-26 08:00:00', :approved => 0},
+
+    
+    {:UIN => 987654321, :car_ride => 0, :comments => "fun fun fun", :wait_listed => 0, :event_id => 4, :time_stamp => '2017-03-26 08:00:00', :approved => 0},
+    {:UIN => 123456789, :car_ride => 0, :comments => "more fun fun fun", :wait_listed => 0, :event_id => 4, :time_stamp => '2017-03-26 19:50:13', :approved => 0},
+
+    
+]
+
+attendances.each do |attendance|
+    Attendance.create!(attendance)
+end
