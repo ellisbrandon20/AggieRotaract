58cb124370d2d75e69eccbb2022d164aea8d80c4
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-04-23 16:49:32 +0000

    display the events in order by date

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
     def index
         curr_time = DateTime.now.to_date
-        @all_events = Event.all
+        @all_events = Event.order(:date).all
         @upcoming_events = @all_events.where("date >= :date and meeting = :meeting", {date: curr_time, meeting: [false]})
         @upcoming_events_email = @upcoming_events
         contact_uin_to_email
