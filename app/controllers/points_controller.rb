94090f56430cd9a7b9efbfd52224ee8604a7f1ce
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-04-24 19:55:48 +0000

    fixed points page error

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,4 +1,29 @@
 class PointsController < ApplicationController
+    # struct
+    UserPoints = Struct.new(:active_record, :event)
+    
+    def index
+
+        @events = Array.new
+
+        #session user uin
+        @users_points = Point.where(UIN: session[:user_uin])
+        #  event_id, points, uin, issue_date    
+        @users_points.each do |user_pt|
+            event = Event.find(user_pt.event_id)
+
+            @events.append(UserPoints.new(user_pt, event))
+        end
+
+
+        if !params[:view_event].nil?
+            @view_event = Event.find(params[:view_event])
+            event_contact = User.find_by(UIN: @view_event.contact)
+            @view_event.contact = event_contact.email 
+        end
+    end
+    
+    
     
     def create
         
