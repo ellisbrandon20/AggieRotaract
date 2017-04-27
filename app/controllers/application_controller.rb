f5394e46ce65dffa225a9c7960d204e1ab4727e1
Brandon Ellis <ellisbrandon20@tamu.edu>
2017-04-27 02:54:50 +0000

    only admin can access admin pages now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper :all
+  helper_method :redirect_to
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
 module ApplicationHelper
 	def flash_class(level)
-	    case level.to_sym
-	    when :notice then "alert alert-info"
-	    when :success then "alert alert-success"
-	    when :error then "alert alert-error"
-	    when :alert then "alert alert-error"
-	    when :danger then "alert alter-danger"
-    end
-  end
+		case level.to_sym
+		    when :notice then "alert alert-info"
+		    when :success then "alert alert-success"
+		    when :error then "alert alert-error"
+		    when :alert then "alert alert-error"
+		    when :danger then "alert alter-danger"
+    	end
+	end
+	
+	def member_redirect
+		if (session[:admin] == false)
+			redirect_to dashboard_index_path
+		end
+	end
 end
